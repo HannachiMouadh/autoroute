c0aa6f146d2e2ec3135cef2a3b5e6d0c4e00828d
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-01-26 01:35:20 +0100

    Update PrivateRouteHome.js

--- front/src/Components/PrivateRoute/PrivateRouteHome.js
+++ front/src/Components/PrivateRoute/PrivateRouteHome.js
@@ -6,7 +6,7 @@ const PrivateRouteHome = () => {
   const isAdmin = localStorage.getItem("isAdmin") === "true"; // Parse admin status properly
 
   // Check if the user is authenticated and an admin
-  return isAuth && isAdmin ? <Outlet /> : <Navigate to="/connection" />;
+  return isAuth || isAdmin ? <Outlet /> : <Navigate to="/connection" />;
 };
 
 export default PrivateRouteHome;
