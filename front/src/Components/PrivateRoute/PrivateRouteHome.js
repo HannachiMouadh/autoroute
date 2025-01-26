877be24314a077d706efcf7515d257a06866640c
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-01-26 01:52:40 +0100

    Update PrivateRouteHome.js

--- front/src/Components/PrivateRoute/PrivateRouteHome.js
+++ front/src/Components/PrivateRoute/PrivateRouteHome.js
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouteHome = () => {
-  const [loading, setLoading] = useState(true);
-  const [isAuth, setIsAuth] = useState(false);
-
-  useEffect(() => {
-    // Simulate token validation from localStorage or an API
-    const token = localStorage.getItem("token");
-
-    if (token) {
-      setIsAuth(true);
-    }
-    setLoading(false); // Mark loading as complete
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>; // Add a spinner or loader component
-  }
+  const isAuth = localStorage.getItem("token");
 
   return isAuth ? <Outlet /> : <Navigate to="/connection" />;
 };
