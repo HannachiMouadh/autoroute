09c66f6e952b645be5a93d4da165c30a7baaf3f3
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-01-26 02:22:02 +0100

    Update PrivateRouteHome.js

--- front/src/Components/PrivateRoute/PrivateRouteHome.js
+++ front/src/Components/PrivateRoute/PrivateRouteHome.js
@@ -3,9 +3,8 @@ import { Navigate, Outlet } from "react-router-dom";
 
 const PrivateRouteHome = () => {
   const isAuth = localStorage.getItem("token");
-  const isAdmin = localStorage.getItem("isAdmin") === "true";
 
-  return isAuth && isAdmin ? <Outlet /> : <Navigate to="/connection" />;
+  return isAuth ? <Outlet /> : <Navigate to="/connection" />;
 };
 
 export default PrivateRouteHome;
