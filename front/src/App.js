a0450f6f3de0ea533beb710d730f5c763a7e55f9
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-01-28 03:37:51 +0100

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,52 @@
-import './App.css';
-import Add from "./Components/Forms/Add";
-import { BrowserRouter as Router,Switch, Route, Routes } from 'react-router-dom';
-import Delete from './Components/Forms/Delete';
-import React from 'react'; 
-import  Home from './Components/Forms/Home.js';
-import Update from './Components/Forms/Update.js';
-import  Tabchange from './Components/Tabchange.js';
-import Signin from './Components/Signin/Signin.js';
-import Signup from './Components/Signup/Signup.js';
-import PrivateRouteHome from './Components/PrivateRoute/PrivateRouteHome.js';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import React, { useEffect } from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Tabchange from "./Components/Tabchange";
+import Signin from "./Components/Signin/Signin";
+import PrivateRouteHome from "./Components/PrivateRoute/PrivateRouteHome";
+import Home from "./Components/Forms/Home";
+import HomeSemaine from "./Components/ParSemaine/HomeSemaine";
+import HomeSens from "./Components/ParSens/HomeSens";
+import HomeHoraire from "./Components/ParHoraire/HomeHoraire";
+import HomeCause from "./Components/ParCause/HomeCause";
+import HomeLieu from "./Components/ParLieu/HomeLieu";
+import Signup from "./Components/Signup/Signup";
+import UsersMan from "./Components/usersMan/UsersMan";
+import { ToastContainer } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+import { currentUser } from "./JS/userSlice/userSlice";
 
-function App() {
+function App({ userRegion,curuser,userCause,userHoraire,userSemaine,userSens,userLieu }) {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(currentUser());
+  }, [dispatch]);
+  
+  const userRedux = useSelector((state) => state.user.user);
+
   return (
-     <div className="App">
-<Tabchange />
-  <Routes>
-  <Route path="/connection" element={<Signin />} />
-  <Route path="/" element={<PrivateRouteHome />}/>
-<Route element={<Tabchange />}/>
-</Routes>
-        <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
+    <div className="App">
+      <Routes>
+        <Route path="/connection" element={<Signin />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRouteHome>
+              <Tabchange />
+            </PrivateRouteHome>
+          }
+        >
+          <Route path="/" element={<Navigate to="/recap" />} />
+          <Route path="/recap" element={<Home userRegion={userRedux.region} curuser={userRedux} />} />
+        <Route path="/par-semaine" element={<HomeSemaine userSemaine={userRedux.region} />} />
+        <Route path="/par-sens" element={<HomeSens userSens={userRedux.region} />} />
+        <Route path="/par-horaire" element={<HomeHoraire userHoraire={userRedux.region} />} />
+        <Route path="/par-cause" element={<HomeCause userCause={userRedux.region} />} />
+        <Route path="/par-lieu" element={<HomeLieu userLieu={userRedux.region} />} />
+        <Route path="/inscription" element={<Signup />} />
+        <Route path="/utilisateurs" element={<UsersMan />} />
+        </Route>
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} hideProgressBar />
     </div>
   );
 }
