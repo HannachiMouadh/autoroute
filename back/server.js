ea524ac3f73e90216e1ad3b81c0d6cc48ea1f1fa
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-03-29 02:06:34 +0100

    Update server.js

--- back/server.js
+++ back/server.js
@@ -4,11 +4,6 @@ const port = 5000;
 require('dotenv').config();
 const DBconnect = require("./DBconnect");
 const cors = require('cors');
-app.use(cors({
-  origin: '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
 
 app.use(cors());
 app.use(express.json());
