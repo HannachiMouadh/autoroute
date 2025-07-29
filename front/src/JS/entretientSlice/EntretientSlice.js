5c81e7b2c0d1db85f424e1e0c993311325fa36e9
HannachiMouadh <67299342+HannachiMouadh@users.noreply.github.com>
2025-07-29 04:02:50 +0100

    Update EntretientSlice.js

--- front/src/JS/entretientSlice/EntretientSlice.js
+++ front/src/JS/entretientSlice/EntretientSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 // Fetch forms action
 export const fetchEntData = createAsyncThunk("data/fetchAll", async () => {
   try {
-    const response = await axios.get("http://localhost:5000/ent/");
+    const response = await axios.get("https://autoroute-api.vercel.app/ent/");
     return response.data.respond;
   } catch (error) {
     console.error("Error fetching entdata:", error);
@@ -15,7 +15,7 @@ export const fetchEntData = createAsyncThunk("data/fetchAll", async () => {
 // Add form action
 export const addEntData = createAsyncThunk("data/add", async (newData) => {
   try {
-    const response = await axios.post("http://localhost:5000/ent/", newData, {
+    const response = await axios.post("https://autoroute-api.vercel.app/ent/", newData, {
       headers: {
         "Content-Type": "application/json",
       },
@@ -30,7 +30,7 @@ export const addEntData = createAsyncThunk("data/add", async (newData) => {
 // Delete form action
 export const deleteEntData = createAsyncThunk("data/delete", async (id) => {
   try {
-    await axios.delete(`http://localhost:5000/ent/${id}`);
+    await axios.delete(`https://autoroute-api.vercel.app/ent/${id}`);
     return id;
   } catch (error) {
     console.error("Error deleting form:", error);
@@ -44,7 +44,7 @@ export const updateEntData = createAsyncThunk(
   async ({ id, entData }, { rejectWithValue }) => {
     try {
       const response = await axios.put(
-        `http://localhost:5000/ent/${id}`,
+        `https://autoroute-api.vercel.app/ent/${id}`,
         entData,
         {
           headers: {
@@ -63,7 +63,7 @@ export const updateEntData = createAsyncThunk(
 export const uploadPhoto = createAsyncThunk(
   "upload/photo",
   async (formDataUpload) => {
-    const response = await axios.post("http://localhost:5000/api/upload", formDataUpload, {
+    const response = await axios.post("https://autoroute-api.vercel.app/api/upload", formDataUpload, {
       headers: {
         "Content-Type": "multipart/form-data", // ✅ critical
       },
