f0f00930e27c4b8d2ab09cdd5646dc3dc7179625
HannachiMouadh <mouadh40@gmail.com>
2026-07-06 00:20:45 +0100

    start1

---
+++ back/server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+require('dotenv').config();
+const DBconnect = require("./DBconnect");
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'https://autoroute-front.vercel.app',
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization', 'app-type', 'Origin', 'X-Requested-With', 'Accept'],
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS']
+};
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
+app.use("/uploads", express.static("uploads"));
+app.use(express.json());
+app.use("/api/user",require('./routes/user'));
+app.use("/auto",require("./routes/autoroute"));
+app.use("/ent",require("./routes/entretient"));
+app.use("/pat",require("./routes/patrouille"));
+app.use("/matPat",require("./routes/matriculePatrouille"));
+app.use("/api/upload",require("./routes/upload"));
+app.use("/api/uploadSingle",require("./routes/uploadSingle"));
+app.use("/api/updatePhoto",require("./routes/updatePhoto"));
+
+mongoose.set('strictQuery', true);
+DBconnect().then(() => {
+  app.listen(port, () => console.log(`Server running on port ${port}`));
+});
